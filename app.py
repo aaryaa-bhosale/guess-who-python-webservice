cedebea5abbf5248f7f7a89f75ca1951ab79c92d
mangesh22121992 <mangesh.khude@infobeans.com>
2019-03-01 19:31:21 +0530

    added login and auth api calls accordingly

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from libs import user_ops
 app = Flask(__name__)
 users_seen = {}
 
+LOGIN_TOKEN = []
 
 @app.route('/api/user/GetUserResults/<limit>', methods=['POST'])
 def get_user_controller(limit):
@@ -97,7 +98,11 @@ def add_user_score_controller():
     print(req_obj)
 
     user_email_id = req_obj['UserEmailID']
-    user_token = req_obj['UserToken']
+    if not verify_generate_token(user_email_id):
+        generate_login_token(user_email_id)
+        user_token = LOGIN_TOKEN
+    else:
+        user_token = req_obj['UserToken']
     test_duration = str(req_obj['TestDuration'])
     question_count = str(req_obj['QuestionsCount'])
     reward_points = str(req_obj['RewardPoints'])
@@ -242,13 +247,42 @@ def get_top_score():
         error_dict = {"Status": "Failure", "Message": "Failed to fetch Top score for the user"}
         return jsonify(error_dict)
 
+
 def validate_token(email, token):
     url = 'http://192.168.2.81:30848/api/login/auth'
-    data_dict = {"EmailID": str(email), "Token":str(token)}
+    if type(token) is list:
+        data_dict = {"EmailID": str(email), "Token":token[-1]}
+    else:
+        data_dict = {"EmailID": str(email), "Token": token}
     print(data_dict)
+
+    #json_data = json.dumps(data_dict)
     resp = requests.post(url, json=data_dict, headers={'Content-Type':'application/json'})
     final_response = resp.json()
     return final_response['user']['token']
+
+
+def generate_login_token(email):
+    login_url = 'http://192.168.2.81:30848/api/login/login'
+    login_dict = {'User': {'EmailID': str(email)}}
+    print(login_dict)
+    login_resp = requests.post(login_url, json=login_dict, headers={'Content-Type': 'application/json'})
+    login_resp_json = login_resp.json()
+    print(login_resp_json)
+    LOGIN_TOKEN.append(login_resp_json['user']['token'])
+    print(type(LOGIN_TOKEN))
+
+
+def verify_generate_token(email):
+    login_token_key_query = "SELECT * FROM userauth_db.user where EmailID = '%s'" % email
+    db_obj = user_ops.get_db_obj()
+    login_token_key = db_obj.db_select_query(login_token_key_query)
+    print(login_token_key)
+    if not login_token_key:
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
