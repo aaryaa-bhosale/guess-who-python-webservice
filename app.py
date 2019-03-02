ef141a23bdd4082f9dd068f41a33a3fb4f4b8834
mangesh22121992 <mangesh.khude@infobeans.com>
2019-03-02 17:52:59 +0530

    final

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def get_user_controller(limit):
     number_of_records = req_obj['NumberOfRecords']
     
     verified_token = validate_token(user_email_id, user_token)
-    if not verified_token == user_token :
+    if not verified_token['token'] == user_token :
         invalid_token = {
             'status': False,
             'message': 'Invalid Token',
@@ -77,7 +77,7 @@ def get_user_controller(limit):
 
     # Database connection and data according to Query
     db_obj = user_ops.get_db_obj()
-    query = "SELECT * FROM score_db.`user_score` where UserEmailId = '%s' LIMIT " +limit
+    query = "SELECT * FROM score_db.`user_score` where UserEmailId = '%s' order by TestID desc LIMIT " +limit
     query = query %user_email_id
     results = db_obj.db_select_query(query)
 
@@ -98,11 +98,7 @@ def add_user_score_controller():
     print(req_obj)
 
     user_email_id = req_obj['UserEmailID']
-    if not verify_generate_token(user_email_id):
-        generate_login_token(user_email_id)
-        user_token = LOGIN_TOKEN
-    else:
-        user_token = req_obj['UserToken']
+    user_token = req_obj['UserToken']
     test_duration = str(req_obj['TestDuration'])
     question_count = str(req_obj['QuestionsCount'])
     reward_points = str(req_obj['RewardPoints'])
@@ -115,7 +111,7 @@ def add_user_score_controller():
     VALUES ('mangesh.khude@infobeans.com',60,'2019-02-19 00:00:00',60,10,6,4)
     """
     verified_token = validate_token(user_email_id, user_token)
-    if not verified_token == user_token:
+    if not verified_token['token'] == user_token:
         invalid_token = {
             'status': False,
             'message': 'Invalid Token',
@@ -219,9 +215,9 @@ def get_top_score():
     2. select max(RewardPoints) from  score_db.user_score where UserEmailID='monika.agrawal@infobeans.com' group by UserEmailID
     3. RewardPoints from  score_db.user_score where UserEmailID='monika.agrawal@infobeans.com' order by TestID desc Limit 1
     """
-    overall_high_score_query = "select max(RewardPoints) from  score_db.user_score group by UserEmailID  LIMIT 1"
-    current_score_false_query = "select max(RewardPoints) from  score_db.user_score where UserEmailID='"+user_email_id+"' group by UserEmailID"
-    current_score_true_query = "select RewardPoints from  score_db.user_score where UserEmailID='"+user_email_id+"' order by TestID desc Limit 1"
+    overall_high_score_query = "select max(RewardPoints) as reward from  score_db.user_score group by UserEmailID  LIMIT 1"
+    current_score_false_query = "select max(RewardPoints) as reward from  score_db.user_score where UserEmailID='"+user_email_id+"' group by UserEmailID"
+    current_score_true_query = "select RewardPoints as reward from  score_db.user_score where UserEmailID='"+user_email_id+"' order by TestID desc Limit 1"
     db_obj = user_ops.get_db_obj()
 
     overall_high_score = db_obj.db_select_query(overall_high_score_query)
@@ -232,39 +228,35 @@ def get_top_score():
             new_data = [{}]
             new_data[0]['Status'] = "Success"
             new_data[0]['Message'] = "User did not played any quiz(Fresh User)"
-            new_data[0]['UserEmailID'] = auth_resp['emailID']
-            new_data[0]['UserToken'] = auth_resp['token']
             new_data[0]['OverallHighScore'] = 0
             new_data[0]['YourScore'] = 0
             if current_score_true:
-                new_data[0]['YourScore'] = current_score_true[0]['RewardPoints']
+                new_data[0]['YourScore'] = current_score_true[0]['reward']
             result_json = jsonify(new_data)
             return result_json
         else:
-            overall_high_score[0]['Status'] = "Success"
-            overall_high_score[0]['Message'] = "Overall High Score retrieved Successfully"
-            overall_high_score[0]['OverallHighScore'] = overall_high_score[0]['max(RewardPoints)']
-            overall_high_score[0]['YourScore'] = 0
-            if current_score_true:
-                overall_high_score[0]['YourScore'] = current_score_true[0]['RewardPoints']
-            result_json = jsonify(overall_high_score)
+            new_data = [{}]
+            new_data[0]['Status'] = "Success"
+            new_data[0]['Message'] = "Overall High Score retrieved Successfully"
+            new_data[0]['OverallHighScore'] = overall_high_score[0]['reward']
+            new_data[0]['YourScore'] = current_score_true[0]['reward']
+            result_json = jsonify(new_data)
             return result_json
     elif is_current_score == 'false':
         if not current_score_false:
             new_data = [{}]
             new_data[0]['Status'] = "Success"
             new_data[0]['Message'] = "User did not played any quiz(Fresh User)"
-            new_data[0]['UserEmailID'] = auth_resp['emailID']
-            new_data[0]['UserToken'] = auth_resp['token']
             new_data[0]['OverallHighScore'] = 0
             new_data[0]['YourScore'] = 0
             result_json = jsonify(new_data)
             return result_json
-        elif current_score_false:
+        else:
             new_data = [{}]
             new_data[0]['Status'] = "Success"
-            new_data[0]['OverallHighScore'] = overall_high_score[0]['max(RewardPoints)']
-            new_data[0]['YourScore'] = 0
+            new_data[0]['Message'] = "Overall High Score retrieved Successfully"
+            new_data[0]['OverallHighScore'] = overall_high_score[0]['reward']
+            new_data[0]['YourScore'] = current_score_false[0]['reward']
             result_json = jsonify(new_data)
             return result_json
     else:
@@ -273,7 +265,8 @@ def get_top_score():
 
 
 def validate_token(email, token):
-    url = 'http://192.168.2.81:30848/api/login/auth'
+    #url = 'http://192.168.2.81:30848/api/login/auth'
+    url = 'http://guesswhodotnet/api/login/auth'
     if type(token) is list:
         data_dict = {"EmailID": str(email), "Token":token[-1]}
     else:
@@ -284,28 +277,6 @@ def validate_token(email, token):
     resp = requests.post(url, json=data_dict, headers={'Content-Type':'application/json'})
     final_response = resp.json()
     return final_response['user']
-
-
-def generate_login_token(email):
-    login_url = 'http://192.168.2.81:30848/api/login/login'
-    login_dict = {'User': {'EmailID': str(email)}}
-    print(login_dict)
-    login_resp = requests.post(login_url, json=login_dict, headers={'Content-Type': 'application/json'})
-    login_resp_json = login_resp.json()
-    print(login_resp_json)
-    LOGIN_TOKEN.append(login_resp_json['user']['token'])
-    print(type(LOGIN_TOKEN))
-
-
-def verify_generate_token(email):
-    login_token_key_query = "SELECT * FROM userauth_db.user where EmailID = '%s'" % email
-    db_obj = user_ops.get_db_obj()
-    login_token_key = db_obj.db_select_query(login_token_key_query)
-    print(login_token_key)
-    if not login_token_key:
-        return True
-    else:
-        return False
 
 
 if __name__ == '__main__':
